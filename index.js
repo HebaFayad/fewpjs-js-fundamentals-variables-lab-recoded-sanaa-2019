66136884cb44231a09e30449d999c091d4f618f4
Heba Fayad <heba.fayad.dobai@gmail.com>
2019-10-19 15:16:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Code your solution in this file!
 const companyName = 'Scuber';
+<<<<<<< HEAD
 let mostProfitableNeighborhood ='Chelsea';
 let companyCeo ='Susan Smith';
+=======
+let mostProfitableNeighborhood ='Chelsea';
+>>>>>>> 3de2329da8f76cbbdc131a4b0e1a5cae79418b3d
